63830c21a0821cbedc636f1b75d072001c613031
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-08-05 02:01:32 +0300

    task 01

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const itemEl = document.querySelectorAll('.item');
+console.log('Number of categories: ', itemEl.length);
+
+const titleEl = document.querySelectorAll('h2');
+console.log('Category: ', titleEl[0].textContent);
+
+console.log('Category: ', titleEl[1].textContent);
+console.log('Category: ', titleEl[2].textContent);
+
+
+
