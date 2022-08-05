764247e2ecb9c9b2743c08de871134825bd619df
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-08-05 14:42:32 +0300

    add change to task 1

--- js/task-01.js
+++ js/task-01.js
@@ -3,9 +3,16 @@ console.log('Number of categories: ', itemEl.length);
 
 const titleEl = document.querySelectorAll('h2');
 console.log('Category: ', titleEl[0].textContent);
-
 console.log('Category: ', titleEl[1].textContent);
 console.log('Category: ', titleEl[2].textContent);
+
+const itemUlEl = document.querySelectorAll('.item ul');
+console.log(itemUlEl)
+
+const liEl = itemUlEl[0].querySelectorAll('li');
+
+console.log('Elements: ', liEl.length);
+
 
 
 
