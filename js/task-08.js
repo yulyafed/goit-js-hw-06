b3844f94a6e048e1849096017660e031134d33b8
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-08-06 17:13:55 +0300

    add task 8.9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) { 
+    event.preventDefault();
+
+    const formElem = event.currentTarget.elements;
+    const email = formElem.email.value;
+    const password = formElem.password.value;
+    
+    if (email === "" ) {
+        alert('Введіть електронну пошту');
+        return;
+    }
+
+    if (password === "" ) {
+        alert('Введіть пароль');
+        return;
+    }
+    
+    const formData = { email, password };
+    console.log(formData);  
+    formEl.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const btnEl = document.querySelector(".change-color");
+const spanEl = document.querySelector('.color');
+console.log(spanEl)
+
+btnEl.addEventListener('click', onBtnClick);
+
+function onBtnClick(event) { 
+  document.body.style.backgroundColor;
+  // spanEl.textContent = 
 }
+
+//   function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
