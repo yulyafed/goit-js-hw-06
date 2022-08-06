1b11bf842ce5672160e0493df3fb9250550e7eca
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-08-07 00:28:09 +0300

    add change in task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,16 +1,16 @@
 const btnEl = document.querySelector(".change-color");
 const spanEl = document.querySelector('.color');
-console.log(spanEl)
 
 btnEl.addEventListener('click', onBtnClick);
 
-function onBtnClick(event) { 
-  document.body.style.backgroundColor;
-  // spanEl.textContent = 
+  function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
-//   function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+function onBtnClick(event) { 
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
+
