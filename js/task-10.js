e77db32db1940b767b0d5a38d12faa3be3e3e801
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-08-07 15:03:24 +0300

    add 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
+const inputEl = document.querySelector('[type="number"]');
+const btnCreateEl = document.querySelector('[data-create]');
+const btnDestroyEl = document.querySelector('[data-destroy]');
+const divBoxesEl = document.querySelector('#boxes');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+ const divEl = document.createElement('div');
+  divEl.width = '-30px'; 
+  divEl.height = '30px';
+  divEl.style.backgroundColor = getRandomHexColor();
+divBoxesEl.append(divEl);
+// console.log(divEl)
+btnCreateEl.addEventListener('click', createBoxes); 
+ 
+function createBoxes(amount) { 
+ 
+  }
+
+btnDestroyEl.addEventListener('click', destroyBoxes);
+
+function destroyBoxes(){ 
+  divEl.remove();
+}
+
+  
